17eae16a8695f03450a1ad4df8871dfa029db129
nikchum <nchumak94@gmail.com>
2023-01-27 15:51:40 +0200

    add start files

---
+++ gulp/tasks/js.js
@@ -0,0 +1,25 @@
+import webpack from "webpack-stream";
+
+export const js = () => {
+  return app.gulp
+    .src(app.path.src.js, { sourcemaps: app.isDev })
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "JS",
+          message: "Error: <%= error.message %>",
+        })
+      )
+    )
+    .pipe(
+      webpack({
+        // mode: "development",
+        mode: app.isBuild ? "production" : "development",
+        output: {
+          filename: "app.min.js",
+        },
+      })
+    )
+    .pipe(app.gulp.dest(app.path.build.js)) // Не сжатый файл js
+    .pipe(app.plugins.browsersync.stream());
+};
